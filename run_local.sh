4b5cca019d6029dbb9096e6306707f2b3de13516
Raymond Lo <raymond.lo@samsungnext.com>
2020-01-29 14:43:09 -0800

    update run local to include task auto creation

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,5 @@
+#generate the jobs
+python3 job_generator.py
 #run one job locally - this will repeat exactly on disco server
 docker run -it -v `pwd`:/local raymondlo84/disco_blender /local/blender_core.py /local/classroom_sample/job_1_1.txt
 
