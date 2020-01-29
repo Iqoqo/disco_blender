e4bcd56e7f601a6103da04c68d0979393657e2e4
Raymond Lo <raymond.lo@samsungnext.com>
2020-01-29 14:51:58 -0800

    Dis.co Cloud

--- run_disco.sh
+++ run_disco.sh
@@ -8,11 +8,11 @@ pip3 install disco --upgrade
 #generate the jobs
 python job_generator.py
 
-#run on AWS
-disco job create -n "blender_720_disco_10" -cit l -s "blender_core.py" -i "classroom_sample/job_*" -r 
+#run on DiscoCloud (Default Cloud)
+#disco job create -n "blender_720_disco_10" -cit l -s "blender_core.py" -i "classroom_sample/job_*" -r 
 
-#run on packet
-#disco job create -n "blender_720_10" -cid 5dd58cd71bc491000d3ea2be -cit l -s "blender_core.py" -i "classroom_sample/job_*" -r
+#run on packet (need a Packet account to be linked in prior)
+#disco job create -n "blender_720_packet_10" -cid 5dd58cd71bc491000d3ea2be -cit l -s "blender_core.py" -i "classroom_sample/job_*" -r
 
 echo "End time"
 date
