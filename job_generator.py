70e84f143783eb3d170b00b7d5e70c249acc4ba9
Raymond Lo <raymond.lo@samsungnext.com>
2020-02-10 14:27:46 -0800

    job generator default enable upload

--- job_generator.py
+++ job_generator.py
@@ -10,8 +10,8 @@ job_name = "job_"
 #url = "http://100.25.247.222/uploads/classroom4k.zip"
 url = "http://100.25.247.222/uploads/classroom_720.zip"
 blend_file = "classroom/classroom.blend"
-#upload_web = "http://100.25.247.222/uploads/upload_images.php"
-upload_web = "" 
+upload_web = "http://100.25.247.222/uploads/upload_images.php"
+#upload_web = "" 
 
 if not os.path.exists(directory):
     os.makedirs(directory)
@@ -19,7 +19,7 @@ if not os.path.exists(directory):
 for i in range (start, end):
     range_in = i*skip+1
     range_out = (i+1)*skip
-    f = open(directory+job_name+str(range_in)+"_"+str(range_out)+".txt", "w+")
+    f = open(directory+job_name+str(range_in)+"_"+str(range_out)+".txt", "w")
     print("File written: "+job_name+str(range_in)+"_"+str(range_out)+".txt")
     f.write(url+"\n")
     f.write(blend_file+"\n")
