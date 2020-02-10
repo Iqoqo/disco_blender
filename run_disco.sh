b147cea64e1a9ac05b52037f7a8d508278f61330
Raymond Lo <raymond.lo@samsungnext.com>
2020-02-10 14:26:35 -0800

    run disco enable default

--- run_disco.sh
+++ run_disco.sh
@@ -9,7 +9,7 @@ pip3 install disco --upgrade
 python job_generator.py
 
 #run on DiscoCloud (Default Cloud)
-#disco job create -n "blender_720_disco_10" -cit l -s "blender_core.py" -i "classroom_sample/job_*" -r 
+disco job create -n "blender_720_disco_10" -cit l -s "blender_core.py" -i "classroom_sample/job_*" -r 
 
 #run on packet (need a Packet account to be linked in prior)
 #disco job create -n "blender_720_packet_10" -cid 5dd58cd71bc491000d3ea2be -cit l -s "blender_core.py" -i "classroom_sample/job_*" -r
